d488127283be4cf5f6c549f131c50acb2e22a70a
MDJ35 <vynceian.oani@hcdc.edu.ph>
2025-09-19 18:45:45 +0800

    sample

--- src/styles/Home.js
+++ src/styles/Home.js
@@ -129,7 +129,7 @@ export const Hero = styled.section`
   }
 
   @media (max-width: 480px) {
-    background-size: 150%; /* Further zoom out for smaller devices */
+    background-size: 105%; /* Further zoom out for smaller devices */
   }
 `;
 
